60e1d25a77a64ec1c266a34c66091ed0d927970d
Amandeep <145090954+amandeep-boot@users.noreply.github.com>
2025-05-18 15:59:21 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ const App = () => {
   const handleTranslate = async (text) => {
     try {
       const response = await fetch("https://text-translator-hjzx.vercel.app/translate", {
-      const response = await fetch("https://text-translator-hjzx.vercel.app//translate", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
